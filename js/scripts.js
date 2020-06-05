72311425655a326cf8a0b9633c9ca8242b93d8e2
Michael Watts <Wattsjmichael@gmail.com>
2020-06-05 08:52:06 -0700

    add function (favoriteFood) to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#coding").submit(function(event){
+    const favoriteFood = $("select#favoriteFood").val();
+
+    if (favoriteFood === "pizza") {
+      $("#rubyAnswer").show();
+    } else if (favoriteFood === "brusselSprouts") {
+      $("#cSharpAnswer").show();
+    } else {
+      $("#javascriptAnswer")
+    }
+
+    event.preventDefault();
+  });
+});
