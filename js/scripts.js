c280646010dc76573b57aa27960d68b76386ae3b
Michael Watts <Wattsjmichael@gmail.com>
2020-06-05 10:17:37 -0700

    change questions in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("form#coding").submit(function(event){
     const favoriteFood = $("select#favoriteFood").val();
 
-    if (favoriteFood === "pizza") {
+    if (favoriteFood == "pizza") {
       $("#rubyAnswer").show();
-    } else if (favoriteFood === "brusselSprouts") {
+    } else if (favoriteFood == "brusselSprouts") {
       $("#cSharpAnswer").show();
     } else {
       $("#javascriptAnswer").show();
