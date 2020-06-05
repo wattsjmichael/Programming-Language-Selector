7c1acb06f3726c5f0b96379b08e3894b40347b08
Michael Watts <Wattsjmichael@gmail.com>
2020-06-05 11:55:40 -0700

    added stop sign question to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
     const age = parseInt($("input#age").val());
     const food = $("select#food").val();
     const favOS = $("select#favOS").val();
+    const sign = $("input:radio[name=sign]:checked").val();
 
-  if (age >=0 && food === "pizza" || favOS === "mac") {
+  if (age >=34 && food === "pizza" || favOS === "mac") {
     $("#ruby").show();
-  } else if (age >=35|| food === "pizza" && favOS === "win") {
+  } else if (age >=35) {
     $("#cSharp").show();
-  } else if (age <=20 && food === "notPizza" && favOS === "huh") {
+  } else if (age <=20 && food === "notPizza") {
     $("#javascript").show();
-  }
+  } else if (sign === "notStopSign")
+    $("#driving").show();
 
   event.preventDefault();
   });
